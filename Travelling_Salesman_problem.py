3ee0942808b7371f0268e08ada43e5bcbb99d316
pratham-2004 <ps.pratham04@gmail.com>
2024-02-18 11:40:49 +0530

    update

--- Travelling_Salesman_problem.py
+++ Travelling_Salesman_problem.py
@@ -3,16 +3,25 @@ from itertools import permutations
 
 v=4;
 
-def TSP(graph){
-    s=int(input("Enter the node where you want to start"))
+def TSP(graph,s):
     vertex=[]
     for i in range(v):
         if i!=s:
-            vertex.append()
+            vertex.append(i)
 
     min_path=maxsize
     new_path=permutations(vertex)
     for j in new_path:
         currentpath_size=0
+        k=s
+        for l in j:
+            currentpath_size+=graph[k][l]
+            k=l
+        currentpath_size+=graph[k][s]
 
-}
+        min_path=min(min_path,currentpath_size)
+    return min_path
+
+graph=[[0,10,15,20],[10,0,35,25],[15,35,0,30],[20,25,30,0]]
+s=0
+print(TSP(graph,s))
